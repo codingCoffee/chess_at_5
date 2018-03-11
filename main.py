246a7d19239e334f76e195bfd641633181e49b74
Ameya Shenoy <shenoy.ameya@gmail.com>
2018-03-11 06:48:36 +0530

    minor fixes

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import os, argparse, cv2
 import sys
 
 
-
 def calc_w_and_b(image):
 	white = 0
 	black = 0
@@ -19,6 +18,8 @@ def does_piece_exists(image):
 	else: return 'T'
 
 def thresh_to_fen():
+	os.system('clear')
+	print('Rough Estimated Piece Detectection')
 	chess_board = cv2.imread('thresh_chess.png', cv2.IMREAD_GRAYSCALE)
 	square_side = int(chess_board.shape[0]/8)
 	old_x = 0
@@ -43,7 +44,10 @@ def cropped_thresh_piece():
 
 	chess_board = cv2.cvtColor(chess_board, cv2.COLOR_BGR2GRAY)
 	chess_board = cv2.GaussianBlur(chess_board, (5,5), 0)
-	_, CHESS_THRESH = cv2.threshold(chess_board,127,255,cv2.THRESH_BINARY)
+	cv2.imshow('image',chess_board)
+	cv2.waitKey(0)
+	cv2.destroyAllWindows()
+	_, chess_board = cv2.threshold(chess_board,127,255,cv2.THRESH_BINARY)
 
 	cv2.imshow('image',chess_board)
 	cv2.waitKey(0)
@@ -57,14 +61,12 @@ if __name__ == "__main__":
 	'Cant come up with anything')
 	p.add_argument('--input', type=str, \
 			help='input image absolute path (default: input.jpg)')
-	p.add_argument('--output', type=str, \
-			help='output path absolute path (default: output.jpg)')
 
 	args = p.parse_args()
-	os.system('cd ../neural-chessboard && python main.py detect --input '+str(args.input)+' --output '+str(args.output))
+	os.system('cd ../neural-chessboard && python main.py detect --input '+str(args.input)+' --output /home/coding_coffee/Projects/hackathon-18/HACK/chess_at_5/cropped.jpg ')
 
 	cropped_thresh_piece()
 
 '''
-python main.py --input /home/coding_coffee/Projects/hackathon-18/HACK/neural-chessboard/test/in/1.jpg --output /home/coding_coffee/Projects/hackathon-18/HACK/chess_at_5/cropped.jpg
+python main.py --input /home/coding_coffee/Projects/hackathon-18/HACK/neural-chessboard/test/in/1.jpg
 '''
